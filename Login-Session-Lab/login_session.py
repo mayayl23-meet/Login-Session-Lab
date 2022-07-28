b722c4d1bd001e8a1f3f7a1235b435ee716dc915
mayayl23-meet <109899357+mayayl23-meet@users.noreply.github.com>
2022-07-28 12:44:09 +0300

    Update login_session.py

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -12,8 +12,8 @@ author = ''
 def home():
 	if request.method == 'POST':
 		try:
-			login_session['age'] = request.form['age']
 			login_session['quote'] = request.form['quote']
+			login_session['age'] = request.form['age']
 			login_session['author'] = request.form['author']
 			return render_template('thanks.html')
 		
