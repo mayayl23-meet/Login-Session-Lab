8e2d2ca5c6378dcbc80cefefadfb7b53a1e10a94
mayayl23-meet <109899357+mayayl23-meet@users.noreply.github.com>
2022-07-28 12:19:51 +0300

    Login Session - Update

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask import session as login_session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-secret-key'
+app.config['SECRET_KEY'] = 'oqihqdoh91370uhwq'
 
-@app.route('/', ) # What methods are needed?
+age = ''
+quote = ''
+author = ''
+
+@app.route('/', methods=['GET', 'POST']) # What methods are needed?
 def home():
+	if request.method == 'POST':
+		try:
+			login_session['age'] = request.form['age']
+			login_session['quote'] = request.form['quote']
+			login_session['author'] = request.form['author']
+			return render_template('thanks.html')
+		
+		except:
+			return render_template('error.html')
 	
 	return render_template('home.html')
 
